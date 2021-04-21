3e6565923487445aed873a93885c690fdf7a91c4
Холодилов Александр <shurikhol@mail.ru>
2021-04-21 16:28:04 +0300

    Прогрессивная автоматизация

--- build/js/main.js
+++ build/js/main.js
@@ -8,7 +8,9 @@
   const cardButton = document.querySelector('.card__button');
   const cart = document.querySelector('.cart');
   const faqQuestions = document.querySelector('.faq__questions');
+  const faqQuestionArray = document.querySelectorAll('.faq__question');
   const galleryFilter = document.querySelector('.gallery__filter');
+  const galleryItems = document.querySelectorAll('.gallery__item');
 
   let onClickMainHeaderLogin;
   let onClickCardButton;
@@ -154,7 +156,10 @@
     cardButton.addEventListener('click', onClickCardButton);
   }
 
-  if (faqQuestions) {
+  if (faqQuestions && faqQuestionArray) {
+    for (let i = 0; i < faqQuestionArray.length; i++) {
+      faqQuestionArray[i].classList.add('faq__question--closed');
+    }
 
     const onClickFaqQuestion = function (event) {
 
@@ -173,7 +178,10 @@
 
   }
 
-  if (galleryFilter) {
+  if (galleryFilter && galleryItems) {
+    for (let i = 0; i < galleryItems.length; i++) {
+      galleryItems[i].classList.add('gallery__item--closed');
+    }
 
     const onClickGalleryFilter = function (event) {
 
